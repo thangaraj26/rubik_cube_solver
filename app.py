cd63e7cc740cdefd239718b1ef4a6a194d7fa29f
Thanga Raj <darkshadow@Thangas-Mac-mini.local>
2022-04-13 10:21:05 +0530

    recently updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify,request,render_template,flash,request, redirect
 from rubikscubetracker import RubiksImage
 from rubik_solver import utils
 import numpy as np 
-
+import os
 app = Flask(__name__)
 
 ###
@@ -20,29 +20,6 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -91,4 +68,5 @@ def api_solve():
         return jsonify({"status":False})  
         
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ("PORT","5000"))
+    app.run(host="0.0.0.0",debug=False,port=port)
